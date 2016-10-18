fcd51a607ffd83abb2bc588fc7dc5e715fa26d7d
Tristan Menzel <tristan.menzel@readify.net>
2016-10-18 13:44:25 +1000

    Set KnownPaths.SolutionRoot to the correct value.

--- src/Sprachtml.Conventions.Tests/SolutionConventions/Buttons/AllHtmlFilesInSolution.cs
+++ src/Sprachtml.Conventions.Tests/SolutionConventions/Buttons/AllHtmlFilesInSolution.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
 using Conventional;
 using NUnit.Framework;
 using Sprachtml.Conventions.Rules;
@@ -8,10 +10,16 @@ namespace Sprachtml.Conventions.Tests.SolutionConventions.Buttons
     [TestFixture]
     public class AllHtmlFilesInSolution
     {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // Note: This is only required until my PR for conventional is merged.
+            KnownPaths.SolutionRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, @"..\..\..\"));
+        }
+
         [Test]
         public void ShouldHaveAnExplicitTypeAttribute()
         {
-            // TODO: sort out why 'ThisSolution' is not working 
             var convention = new HtmlConvention(new[] {new ButtonMustSpecifyTypeAttribute()});
             ThisSolution.MustConformTo(convention).WithFailureAssertion(Assert.Fail);
         }
