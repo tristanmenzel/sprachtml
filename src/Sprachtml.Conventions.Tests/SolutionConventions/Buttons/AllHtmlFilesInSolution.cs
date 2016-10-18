cefbdf6062bd0912f842bafcebcde85c1e17fa30
Tristan Menzel <tristan.menzel@readify.net>
2016-10-18 15:08:00 +1000

    Update Conventional

--- src/Sprachtml.Conventions.Tests/SolutionConventions/Buttons/AllHtmlFilesInSolution.cs
+++ src/Sprachtml.Conventions.Tests/SolutionConventions/Buttons/AllHtmlFilesInSolution.cs
@@ -10,13 +10,6 @@ namespace Sprachtml.Conventions.Tests.SolutionConventions.Buttons
     [TestFixture]
     public class AllHtmlFilesInSolution
     {
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
-        {
-            // Note: This is only required until my PR for conventional is merged.
-            KnownPaths.SolutionRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, @"..\..\..\"));
-        }
-
         [Test]
         public void ShouldHaveAnExplicitTypeAttribute()
         {
